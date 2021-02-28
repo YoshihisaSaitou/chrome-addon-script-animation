64eee5b965b827c5e46492bb8144d151bed2d984
YoshihisaSaitou <anti_viral_drug@yahoo.co.jp>
2021-02-28 21:47:35 +0900

    Type sound.

--- js/animation_bk20210227.js
+++ js/animation_bk20210227.js
@@ -10,38 +10,14 @@ var text_typing_obj = document.getElementById('text_typing');
 var text_count = 0;
 var text_position = 0;
 var interval_id = 0;
-var interval_count = 0;
-var interval_timeout = 6;
-//var key_sound = new Audio('../../sound/key_sound_p1.mp3');
-var key_sound_list_element = 0;
-var key_sound_list = [
-    new Audio('../../sound/key_sound_p1.mp3'),
-    new Audio('../../sound/key_sound_p2.mp3'),
-    new Audio('../../sound/key_sound_p3.mp3'),
-    new Audio('../../sound/key_sound_p4.mp3'),
-    new Audio('../../sound/key_sound_p5.mp3'),
-];
-
+var key_sound = new Audio('../../sound/key_sound3.wav');
 btn_typing_obj.addEventListener('click', function(){
     console.log('text_list', text_list);
     console.log('text_list.length', text_list.length);
     console.log('text_list[text_list_element].content.length', text_list[text_list_element].content.length);
     
-    //文字数 × ミリ秒 = 総ミリ秒
-    //総ミリ秒 / 1000 = ファイル総秒数
-    //総秒数 × ファイル数 = 総ファイル数の秒数
-    //総ファイル数の秒数 / 60 = 総ファイル数の分数
-    //総ファイル数の分数 / 60 = 総ファイル数の時間
-    
-    //(((文字数 × ミリ秒) / 1000) × 60) × 60 = 総時間
-    //var total_time = (((text_list[text_list_element].content.length * interval_timeout) / 1000) * 60) * 60;
-    //console.log('total_time', total_time);
-    
-    //ファイル選択を非表示
+    //
     document.getElementById('input_box').style.display = 'none';
-    
-    //表示場所を表示
-    document.getElementById('text_typing').style.display = 'block';
     
     //撮影用にインターバルを設ける
     setTimeout(runSetInterval, 1000);
@@ -58,18 +34,11 @@ btn_typing_obj.addEventListener('click', function(){
 
 //タイマー処理実行
 function runSetInterval(){
-    interval_id = setInterval(runTypingAnimation, interval_timeout, text_typing_obj);
+    interval_id = setInterval(runTypingAnimation, 100, text_typing_obj);
 }
 
 //タイピングアニメーション実行
 function runTypingAnimation(disp_obj){
-    /*interval_count++;
-    if(interval_count >= interval_timeout){
-        console.log('interval_count', interval_count);
-        interval_count = 0;
-    }else{
-        //return null;
-    }*/
     //console.log('disp_obj', disp_obj);
     //console.log('text_position', text_position);
     
@@ -143,11 +112,7 @@ function runTypingAnimation(disp_obj){
             //内容を表示中の場合は位置を増やす
             text_position++;
             
-            key_sound_list[key_sound_list_element].play();
-            key_sound_list_element++;
-            if(key_sound_list_element >= key_sound_list.length){
-                key_sound_list_element = 0;
-            }
+            key_sound.play();
         }
         
     }
